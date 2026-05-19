a4a93d99a7da1f646f2da8159f84cd969e352539
harout-19 <126747134+harout-19@users.noreply.github.com>
2026-05-19 00:28:21 -0400

    POST /tasks endpoint is made

--- server.js
+++ server.js
@@ -5,12 +5,26 @@ import db from "./database.js"
 const app = express()
 const PORT = 3000
 
+app.use(express.json())
 
 app.get("/tasks", (req, res) => {
 
     const row = db.prepare(`SELECT * FROM tasks`).all()
     console.log(row)
+
     return res.json(row)
+})
+
+app.post("/tasks", (req, res) => {
+
+    const insert = db.prepare('INSERT INTO tasks (title) VALUES (?)')
+
+    const t = req.body.title
+    const new_data = insert.run(t)
+
+    const result = db.prepare('SELECT * FROM tasks WHERE id = ?').get(new_data.lastInsertRowid)
+
+    return res.json(result)
 })
 
 const startServer = () => {
