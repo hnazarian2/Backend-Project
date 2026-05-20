52e03d615190bed05c0c9c3a39a59de2a8c7f551
harout-19 <126747134+harout-19@users.noreply.github.com>
2026-05-20 18:44:18 -0400

    Backend Project 1 - REST API with CRUD endpoints for task management

--- server.js
+++ server.js
@@ -27,6 +27,29 @@ app.post("/tasks", (req, res) => {
     return res.json(result)
 })
 
+app.delete("/tasks/:id", (req, res) => {
+
+    const ID = req.params.id
+
+    const dlt = db.prepare('DELETE FROM tasks WHERE id = ?')
+    const deleted_data = dlt.run(ID)
+
+    return res.json('~~Deleting Successful~~')
+})
+
+app.put("/tasks/:id", (req,res) => {
+
+    const update_id = req.params.id
+    const updated_title = req.body.title
+
+    const update = db.prepare('UPDATE tasks SET title = ? WHERE id = ?')
+    const updating = update.run(updated_title, update_id)
+
+    const result = db.prepare('SELECT * FROM tasks WHERE id = ?').get(update_id)
+
+    return res.json(result)
+})
+
 const startServer = () => {
     console.log(`http://localhost:${PORT}`)
     console.log(`Press CTRL + C to exit`)
